c78b99fd47d0671f800e9486e6258a66bf3e2b82
colin <linc04232@gmail.com>
2025-04-07 17:39:17 +0700

    1

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -18,21 +18,21 @@
 
 	const getChatUrl = async () => {
 		const res = await getChat()
-		// console.log(res)
-		serviceSrc.value = res.data.url
-		// serviceSrc.value = 'https://chat.tiktokh.shop/#/userChat?userNo=499&nickname=tooy&remark=&csId=7&fromUrl=fromUrl&sign=90CA51E711E74A2C2E0859740A32C46D'
-		// serviceSrc.value = 'http://192.168.31.40:2023/#/userChat?userNo=499&nickname=tooy&remark=&csId=7&fromUrl=fromUrl&sign=90CA51E711E74A2C2E0859740A32C46D'
+
+	 serviceSrc.value = res.data.url
+	// serviceSrc.value = "https://manage.tiktokh.shop/chatlink.html?metadata={%22name%22:%22%E8%80%81%E7%8E%8B%22,%22qq%22:%22123456%22,%22age%22:%2211%22}"
+
 	}
 	onBeforeMount(() => {
 		getChatUrl()
 	})
 </script>
 <template>
-	<header>
+<!--	<header>
 		<van-nav-bar :title="$t('service.service')" :left-text="$t('goback')" left-arrow @click-left="onClickLeft" />
-	</header>
+	</header>-->
 	<div class="iframe">
-		<iframe style="height: 87vh; width: 100%; overflow: hidden;" frameborder="0" id="feee_c2c_widget"
+		<iframe style="height: 93vh; width: 100%; overflow: hidden;" frameborder="0"
 			:src="serviceSrc"></iframe>
 	</div>
 </template>
